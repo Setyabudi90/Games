92ec62b16fa49bc370812887aedd564e3dd469c7
i.setya_b <115447070+Setyabudi90@users.noreply.github.com>
2023-06-28 17:26:44 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import pygame
 import random
 
-# Initialize pygame
 pygame.init()
 
-# Set up the window
 screen_width = 600
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Car Racing Game")
-
-# Load the images
+pygame.display.set_caption("Car Racing")
 car_image = pygame.image.load("car.png")
 car_width = 50
 car_height = 100
@@ -18,7 +14,6 @@ obstacle_image = pygame.image.load("obstacle.png")
 obstacle_width = 50
 obstacle_height = 50
 
-# Set up the game variables
 clock = pygame.time.Clock()
 score = 0
 car_x = screen_width // 2 - car_width // 2
@@ -28,7 +23,6 @@ obstacle_x = random.randint(0, screen_width - obstacle_width)
 obstacle_y = -obstacle_height
 obstacle_speed = 5
 
-# Define the functions
 def update_score():
     font = pygame.font.Font(None, 36)
     score_text = font.render(f"Score: {score}", True, (255, 255, 255))
@@ -46,10 +40,8 @@ def check_collision():
     else:
         return False
 
-# Set up the game loop
 running = True
 while running:
-    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -61,7 +53,6 @@ while running:
         elif event.type == pygame.KEYUP:
             car_speed = 0
 
-    # Update the game state
     score += 1
     car_x += car_speed
     if car_x < 0:
@@ -76,15 +67,12 @@ while running:
     if check_collision():
         running = False
 
-    # Draw the screen
     screen.fill((0, 0, 0))
     update_score()
     draw_car()
     draw_obstacle()
     pygame.display.update()
 
-    # Set the frame rate
     clock.tick(60)
 
-# Clean up the game
 pygame.quit()
